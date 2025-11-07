01dbc69dcfd44190bf4488c92a38e7a61c06c48b
Evan Killion <79597389+evankilli@users.noreply.github.com>
2025-11-06 17:30:18 -0800

    Clean comments

--- Stockholm_transit/stockholm.sql
+++ Stockholm_transit/stockholm.sql
@@ -4,6 +4,7 @@ create table sholm as
   select osm_id, name, way as geom
   from se_polygon
   where admin_level = '7' and name = 'Stockholms kommun'
+-- we need to make sure we're only looking at Stockholm and not adjacent suburbs
 
 --------------------------------------------------------------------------------
 
@@ -13,7 +14,7 @@ create table res_osm as
 select osm_id, building, way as geom
 from se_polygon
 where building = 'yes' or building = 'residential'
--- SELECT
+-- select residences and unclassified buildings. Some mixed use buildings in dense areas are unclassified, and classification of buildings in particular on OpenStreetMap is imperfect, so a decision was made to err on the side of too many rather than too few residences
 
 alter table res_osm
 add column within_sholm INTEGER
@@ -23,42 +24,26 @@ update res_osm
   set within_sholm = 1
   from sholm
   where st_within(res_osm.geom, sholm.geom)
-  -- give value of 1 where within sholm_tram
+  -- give value of 1 where within Stockholm
 
 select within_sholm, count(osm_id)
 from res_osm
 group by within_sholm
--- check
+-- check, works!
 
 delete from res_osm
   where within_sholm is null
-  -- only sholm now!
+  -- only Stockholm now!
 
 --------------------------------------------------------------------------------
 
 /* 3 Isolate transit stops */
 
-/* create table sholm_tram as
-select osm_id
-from "GOD KNOWS WHERE"
-where railway = 'tram_stop'
-
-create table sholm_rail_transit_node as
-select "column"
-from "GNW"
-where railway = 'station' and type = 'node'
-
-create table sholm_rail_transit_area as
-select "column"
-from "GNW"
-where railway = 'station' and type = 'area'
--- option for breaking apart */
-
 create table sholm_rail_transit as
 select osm_id, name, railway, public_transport, way as geom
 from se_point
 where railway = 'tram_stop' or railway = 'station'
--- option for keeping together
+-- Create table of tram stop and railway station points
 
 alter table sholm_rail_transit
 add column within_sholm INTEGER
@@ -73,10 +58,12 @@ update sholm_rail_transit
 select within_sholm, count(osm_id)
 from sholm_rail_transit
 group by within_sholm
--- check
+-- check, all good!
 
 delete from sholm_rail_transit
   where within_sholm is null
+  -- delete where outside of Stockholm
+
 
 ----------- now for all transit ------------------------------------------------
 
@@ -84,6 +71,7 @@ create table sholm_transit as
 select osm_id, name, railway, public_transport, highway, way as geom
 from se_point
 where public_transport = 'stop_position' or public_transport = 'station' or highway = 'bus_stop'
+-- repeat process of creating table of transit stops using expanded parameters
 
 alter table sholm_transit
 add column within_sholm INTEGER
@@ -93,7 +81,7 @@ update sholm_transit
   set within_sholm = 1
   from sholm
   where st_within(sholm_transit.geom, sholm.geom)
-  -- give value of 1 where within sholm_tram
+  -- give value of 1 where within Stockholm
 
 select within_sholm, count(osm_id)
 from sholm_transit
@@ -102,10 +90,13 @@ group by within_sholm
 
 delete from sholm_transit
   where within_sholm is null
+  -- delete transit stops outside of Stockholm
+
 
 select railway, count(osm_id)
   from sholm_transit
   group by railway
+  -- Validate number of railway stops
 --------------------------------------------------------------------------------
 
 /* 4 Isolate stockholm neighborhoods */
@@ -118,12 +109,11 @@ except
 select osm_id, name, population, way as geom
 from se_polygon
 where name = 'Gustavsberg'
--- one admin_levl 9 showed up outside city, this deletes it
-
--- i know neighborhoods are admin level 10
--- imma use Stockholms kommun (admin level 7) as my boundaries
--- i wanna find all the polygons within this so I have stockholm subdivisions
--- looks like admin level 10 only exists within a subset of cities so I should be fine?
+-- Gustavsberg was the only borough/Stadsdelsområde outside of Stockholm which showed up when admin_level 9 was queried, and so needed to be excluded. admin_level 9 is not universal, so admin_level 9 was not assigned to administrative divisions within most areas.
+-- i know neighborhoods are admin level 9
+-- i will use Stockholms kommun (admin level 7) as my boundaries
+-- i want to find all the polygons within this so I have stockholm subdivisions
+-- looks like admin level 9 only exists within a subset of cities so I should be fine
 
 --------------------------------------------------------------------------------
 
@@ -133,6 +123,8 @@ where name = 'Gustavsberg'
 create table res_osm_pt as
 select osm_id, building, ST_CENTROID(geom) as geom
 from res_osm
+-- all transit stops were classified as points
+
 
 --------------------------------------------------------------------------------
 
@@ -147,13 +139,13 @@ create table res_hood as
   from res_osm_pt
   join sholm_hoods
   on st_intersects(sholm_hoods.geom, res_osm_pt.geom)
-  -- creating a table that joins ward id to each residence points
+  -- creating a table that joins neighborhood id to each residence points
 
  create table reshood_cnt as
    select name, count(osm_id)
    from res_hood
    group by name
-  -- creating a table that groups and counts residences in each ward
+  -- creating a table that groups and counts residences in each neighborhood
 
 create table hood_w_res_cnt as
   select
@@ -163,7 +155,7 @@ create table hood_w_res_cnt as
   from reshood_cnt
   join sholm_hoods
   on reshood_cnt.name = sholm_hoods.name
--- joining resward_cnt w ward geometries to get a map of wards with counts of residences
+-- joining reshood_cnt w neighborhood geometries to get a map of neighborhoods with counts of residences
 
 --------------------------------------------------------------------------------
 
@@ -171,41 +163,41 @@ create table hood_w_res_cnt as
 SELECT addgeometrycolumn('evan','res_osm_pt','utmgeom',32737,'POINT',2);
 UPDATE res_osm_pt
 SET utmgeom = ST_Transform(geom, 32737);
---prepare residences for analysis
+--prepare residences for geographic analysis
 
 SELECT addgeometrycolumn('evan','sholm_rail_transit','utmgeom',32737,'POINT',2);
 UPDATE sholm_rail_transit
 SET utmgeom = ST_Transform(geom, 32737);
--- prepare rail for analysis
+-- prepare rail for geographic analysis
 
 
 SELECT addgeometrycolumn('evan','sholm_transit','utmgeom',32737,'POINT',2);
 UPDATE sholm_transit
 SET utmgeom = ST_Transform(geom, 32737);
--- prepare all transit for analysis
+-- prepare all transit for geographic analysis
 
 
 SELECT addgeometrycolumn('evan','hood_w_res_cnt','utmgeom',32737,'POLYGON',2);
 UPDATE hood_w_res_cnt
 SET utmgeom = ST_Transform(geom, 32737);
--- prepare hoods for analysis
+-- prepare neighborhoods for geographic analysis
 
 
 ALTER TABLE res_osm_pt ADD COLUMN access_rail INTEGER;
 ALTER TABLE res_osm_pt ADD COLUMN access_all INTEGER;
--- add access to residential table
+-- add access yes/no columns to residential table
 
 UPDATE res_osm_pt
 SET access_rail = 1
 FROM sholm_rail_transit
 WHERE ST_DWITHIN(res_osm_pt.utmgeom, sholm_rail_transit.utmgeom, 402.34);
--- make access equal one when residence is within 1/4mi or 402.34m of rail transit
+-- make access_rail equal one when residence is within 1/4mi or 402.34m of rail transit
 
 UPDATE res_osm_pt
 SET access_all = 1
 FROM sholm_transit
 WHERE ST_DWITHIN(res_osm_pt.utmgeom, sholm_transit.utmgeom, 402.34);
--- make access equal one when residence is within 1/4mi or 402.34m of any transit
+-- make access_all equal one when residence is within 1/4mi or 402.34m of any transit
 
 select *
 from res_osm_pt
@@ -214,7 +206,7 @@ limit 1000;
 select access_rail, count(osm_id)
 from res_osm_pt
 group by access_rail
--- lets check
+-- lets check, works!
 
 select *
 from res_osm_pt
@@ -223,19 +215,19 @@ limit 1000;
 select access_all, count(osm_id)
 from res_osm_pt
 group by access_all
--- lets check
+-- lets check, works!
 
 CREATE TABLE res_within_rail AS
 SELECT *
 FROM res_osm_pt
 WHERE access_rail = 1
--- table with only residences within buffer
+-- create table with only those residences within buffer of rail transit
 
 CREATE TABLE res_within_all AS
 SELECT *
 FROM res_osm_pt
 WHERE access_all = 1
--- table with only residences within buffer
+-- create table with only those residences within buffer of all transit
 
 --------------------------------------------------------------------------------
 
@@ -249,7 +241,7 @@ create table hoods_w_rail_access as
   join res_within_rail b
   on st_intersects(a.geom, b.geom)
   group by a.hood_name
--- count residences with access in each ward
+-- count residences with access to rail in each neighborhood
 
 create table hoods_w_transit_access as
   select
@@ -258,7 +250,7 @@ create table hoods_w_transit_access as
   join res_within_all b
   on st_intersects(a.geom, b.geom)
   group by a.hood_name
--- how do i do the second join/count???
+  -- count residences with access to all transit in each neighborhood
 
 
 create table hoods_1 as
@@ -270,8 +262,7 @@ create table hoods_1 as
     from hood_w_res_cnt
     full outer join hoods_w_rail_access
     on hood_w_res_cnt.hood_name = hoods_w_rail_access.name
--- join the wards with number with access to the table with the names,
--- total count, and geom
+-- join the neighborhoods with number with access to rail transit to the table with the names, total count, and geom
 
 create table hoods_final as
     select
@@ -283,6 +274,7 @@ create table hoods_final as
     from hoods_1
     join hoods_w_transit_access
     on hoods_1.name = hoods_w_transit_access.name
+-- Perform similar join to get number of residences with access to all transit as well
 
 update hoods_final
   set rail_access = 0
@@ -290,8 +282,7 @@ update hoods_final
 update hoods_final
   set transit_access = 0
   where transit_access is null
--- make it 0 instead of null
--- not needed
+-- make access columns 0 instead of null
 
 ALTER TABLE hoods_final
 ADD COLUMN pct_rail float(8);
